2af154d8a3b267260e69a68456c856aa6fc90fb0
imjoseangel <josea.munoz@gmail.com>
2021-10-20 22:25:12 +0200

    add argparse

--- k8s_diagrams/k8s-diagrams.py
+++ k8s_diagrams/k8s-diagrams.py
@@ -3,8 +3,60 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
-
+import logging
+import sys
+import argparse
 from kubernetes import client, config
+
+FALLBACK_ARGS = dict(namespace='default', filename='k8s', directory='diagrams',
+                     label='Kubernetes',)
+
+
+class ParseArgs():
+    def __init__(self):
+
+        # Parse arguments passed at cli
+        self.parse_arguments()
+
+    def parse_arguments(self):
+
+        description = '''
+
+    ┬┌─┌─┐┌─┐  ┌┬┐┬┌─┐┌─┐┬─┐┌─┐┌┬┐┌─┐
+    ├┴┐├─┤└─┐ ─ │││├─┤│ ┬├┬┘├─┤│││└─┐
+    ┴ ┴└─┘└─┘  ─┴┘┴┴ ┴└─┘┴└─┴ ┴┴ ┴└─┘
+
+    Create diagram from the Kubernetes API.'''
+
+        parser = argparse.ArgumentParser(description=description,
+                                         formatter_class=argparse.RawTextHelpFormatter)
+
+        parser.add_argument('--namespace',
+                            '-n',
+                            help=('The namespace we want to draw. (default: '
+                                  '"default") [$KUBECTL_NAMESPACE]'),
+                            default=FALLBACK_ARGS['namespace'])
+
+        parser.add_argument('--kubeconfig',
+                            '-k',
+                            help='The path to your kube config file. [$KUBECONFIG]')
+
+        parser.add_argument('--filename',
+                            '-f',
+                            help='The output filename. (default: "k8s")',
+                            default=FALLBACK_ARGS['filename'])
+
+        parser.add_argument('--directory',
+                            '-d',
+                            help='The output directory. (default: "diagrams")',
+                            default=FALLBACK_ARGS['directory'])
+
+        parser.add_argument('--label',
+                            '-l',
+                            help='The diagram label. (default: "Kubernetes")',
+                            default=FALLBACK_ARGS['label'])
+
+        self.args = parser.parse_args()
 
 
 class K8sDiagrams():
@@ -21,6 +73,10 @@ class K8sDiagrams():
 
 
 def main():
+
+    options = ParseArgs()
+    logging.basicConfig(format="%(message)s",
+                        stream=sys.stdout, level=logging.INFO)
 
     k8s_diagrams = K8sDiagrams()
     pods = k8s_diagrams.get_pods()
